bd96fbc7cb0f0067b2ad6bfbc90137d26ee4739f
RyanReedKnight <ryanreedknight@pm.me>
2026-06-26 19:19:44 -0700

    IDK

--- src/main.c
+++ src/main.c
@@ -1,8 +1,34 @@
 #include "../include/income_tax.h"
 #include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+
+char oregon_single[] = "stuff/oregon_single.csv";
 
 int main(void)
 {
+    FILE *fptr =fopen(oregon_single,"r");
+
+    if(!fptr)
+    {
+
+        printf("Cant open file. return 1\n");
+        return 1;
+
+    }
+
+
+    char c;
+
+    while((c = fgetc(fptr)) !=EOF)
+    {
+
+
+
+    }
+
+
+    fclose(fptr);
 
     return 0;
 }
